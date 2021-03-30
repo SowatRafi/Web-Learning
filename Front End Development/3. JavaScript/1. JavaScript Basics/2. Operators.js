f764447f9e4d2274db52553337df2d11b039794e
Sowat Hossain Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-03-30 18:48:49 +0600

    Operators in JS.

---
+++ Front End Development/3. JavaScript/1. JavaScript Basics/2. Operators.js
@@ -0,0 +1,15 @@
+"use strict";
+var a = 5;
+var b = '5';
+
+if ( a == b ) {
+    console.log("== is True");
+} else {
+    console.log("== is False");
+}
+
+if ( a === b ) {
+    console.log("=== is True");
+} else {
+    console.log("=== is False");
+}
